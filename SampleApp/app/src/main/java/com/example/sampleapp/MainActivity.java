d462aac190199aa3e468f5bc55ffe30ac12aa5f7
leansevenuteen <22520174@gm.uit.edu.vn>
2024-11-26 17:47:58 +0700

    giao dien

---
+++ SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sampleapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button registerButton = findViewById(R.id.resgisterButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
